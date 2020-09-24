17a9499d5d05462ae4c0394a03fa67a7897eafa8
m4gga <margriette123@gmail.com>
2020-09-24 12:10:28 +0000

    database connection + moved the schemas to schemas not services

---
+++ MansionDeSubastas/data/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const artSchema = require('../schemas/art');
+const artistSchema = require('../schemas/artist');
+const auctionSchema = require('../schemas/auction');
+const auctionBidSchema = require('../schemas/auctionBid');
+const customerSchema = require('../schemas/customer');
+
+
+module.exports = {
+    Art: connection.model('Art', artSchema),
+    Artist: connection.model('Artist', artistSchema),
+    Auction: connection.model('Auction', auctionSchema),
+    AuctionBid: connection.model('AuctionBid', auctionBidSchema),
+    Customer: connection.model('Customer', customerSchema)
+};
